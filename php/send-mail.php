ce0178b9f5c68ef4ed62534a63c557a9127f1c99
ilonav8 <osuch.ilona@gmail.com>
2017-05-05 17:17:47 +0200

    Nav change

--- php/send-mail.php
+++ php/send-mail.php
@@ -7,8 +7,8 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-latest.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="css/style.css">
-  <link href="img/favicon.ico" rel="icon" type="image/x-icon" />
+  <link rel="stylesheet" href="../css/style.css">
+  <link href="../img/favicon.ico" rel="icon" type="image/x-icon" />
 </head>
 
 <body>
@@ -21,17 +21,17 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">Petrolhead</a>
+            <a class="navbar-brand" href="../index.html">Petrolhead</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-              <li><a href="index.html">Main</a></li>
-              <li><a href="models.html">Models</a></li>
-              <li><a href="gallery.html">Gallery</a></li>
-              <li><a href="php/contact.html">Contact</a></li>
+              <li><a href="../index.html">Main</a></li>
+              <li><a href="../models.html">Models</a></li>
+              <li><a href="../gallery.html">Gallery</a></li>
+              <li><a href="../contact.html">Contact</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-            <li><a href="php/cylinder.php">Cylinder</a></li>
+            <li><a href="cylinder.php">Cylinder</a></li>
             </ul>
         </div>
       </div>

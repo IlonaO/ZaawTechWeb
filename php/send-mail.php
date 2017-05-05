b21a08c4faeec677c58524cb2da330798fa87885
ilonav8 <osuch.ilona@gmail.com>
2017-05-05 16:40:31 +0200

    Echo message change

--- php/send-mail.php
+++ php/send-mail.php
@@ -45,7 +45,7 @@
     $from='From: $email\r\nReturn-path: $email'; 
         $subject="Someone from your website is contacting you"; 
         mail('osuch.ilona@gmail.com', $subject, $message, $from);
-    echo "<h1>Email sent!</h1>";
+    echo "<h1>Thank you " + $email + "! Your email has been sent!</h1>";
   ?>
 
 <a class="btn btn-default" href="../index.html" role="button">Return to main page</a>

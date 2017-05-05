1769005822d4d3141ed3699a75d43db7cc5badfb
ilonav8 <osuch.ilona@gmail.com>
2017-05-06 00:00:26 +0200

    Total surface area of cone changed

--- php/cylinder.php
+++ php/cylinder.php
@@ -41,9 +41,9 @@
   <?php
     $cone_radius = $_POST['cone_radius'];
     $cone_height = $_POST['cone_height'];
-    $cone_volume = (M_PI*$cone_radius*$cone_radius*$cone_height)/3;
-    $slant = sqrt($cone_radius*$cone_radius*$cone_height*$cone_height);
-    $cone_surface = M_PI*$cone_radius*$slant+M_PI*($cone_radius*$cone_radius);
+    $cone_volume = (pi()*$cone_radius*$cone_radius*$cone_height)/3;
+    $slant = sqrt($cone_radius*$cone_radius+$cone_height*$cone_height);
+    $cone_surface = pi()*$cone_radius*($slant+$cone_radius);
     echo "<h3>Cone volume: $cone_volume </h3>";
     echo "<h3>Cone surface area: $cone_surface </h3>";
 ?>
